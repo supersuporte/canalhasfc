ba20a459d0791b00c0b691887d76208e72175dc8
Carlos Gomes <supersuporte@gmail.com>
2013-08-26 21:14:16 -0300

    Initial Commit

---
+++ CanalhasFC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CanalhasFC
+//
+//  Created by Carlos Gomes on 26/08/13.
+//  Copyright (c) 2013 Carlos Gomes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CanalhasFC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CanalhasFC
+//
+//  Created by Carlos Gomes on 26/08/13.
+//  Copyright (c) 2013 Carlos Gomes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
